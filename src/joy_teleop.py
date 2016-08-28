7e119672b0542373e596641e48aec4cc2fcbb90f
Abhishek Patil <patilnabhi@gmail.com>
2016-08-28 15:21:54 -0500

    updated urdf w/ gazebo_ros_control to publish joint states

--- src/joy_teleop.py
+++ src/joy_teleop.py
@@ -1,27 +1,42 @@
 #!/usr/bin/env python
 import rospy
+import sys
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
+from gazebo_msgs.msg import LinkStates
+
+class JoyTeleop:
+
+	def __init__(self):
+		rospy.init_node('joy_teleop')
+		self.joy_cmd = Twist()
+		#Setup subscriber
+		self.joy = rospy.Subscriber('joy', Joy, self.joy_callback)
+		# self.joints_states = rospy.Subscriber('joints_states', LinkStates, self.joints_states_callback)
+
+		# Setup publisher
+		self.pub_twist = rospy.Publisher('/cmd_vel', Twist, queue_size=20)
+
+		rate = rospy.Rate(20)
+		while not rospy.is_shutdown():
+			self.pub()
+			rate.sleep()
+
+	def pub(self):
+		self.pub_twist.publish(self.joy_cmd)
+
+	def joy_callback(self, joy):
+
+		self.joy_cmd.linear.x = 0.5 * joy.axes[1]
+		self.joy_cmd.angular.z = 0.5 * joy.axes[0]
 
 
+def main():
+	joy_teleop_node = JoyTeleop()
+	try:
+		rospy.spin()
+	except rospy.ROSInterruptException:
+		pass
 
-def callback(data):
-	twist = Twist()
-	twist.linear.x = 0.5 * data.axes[1]
-	twist.angular.z = 0.5 * data.axes[0]
-	pub.publish(twist)
- 
-def joy_teleop():
-	rospy.init_node('Joy_teleop')
-	rospy.Subscriber("joy", Joy, callback)
-	global pub
-	pub = rospy.Publisher('/cmd_vel', Twist, queue_size=20)
-	r = rospy.Rate(10) # 10hz
-	while not rospy.is_shutdown():
-		r.sleep()
- 
-	# spin() simply keeps python from exiting until this node is stopped
-	rospy.spin()
- 
 if __name__ == '__main__':
-	joy_teleop()
+	sys.exit(main())
