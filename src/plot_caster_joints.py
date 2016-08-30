8f4371a97bfbe0a6a4f4d570e051f43997c3dd43
Abhishek Patil <patilnabhi@gmail.com>
2016-08-30 12:18:16 -0500

    updated w/ sim & est plots

--- src/plot_caster_joints.py
+++ src/plot_caster_joints.py
@@ -6,6 +6,9 @@ from std_msgs.msg import Float32
 from geometry_msgs.msg import Twist
 import random
 import matplotlib.pyplot as plt
+from math import sin, cos, atan2
+import numpy as np
+from scipy.integrate import odeint
 
 class PlotCasterJoints:
 
@@ -19,10 +22,16 @@ class PlotCasterJoints:
         self.r_caster_angle = Float32()
         self.wheel_cmd = Twist()
 
+        # time to move wheelchair 
         self.move_time = 3.0
+
+        self.rate = 100
 
         self.l_caster_data = []
         self.r_caster_data = []
+
+        # wheelchair constants
+        self.wh_consts = [0.58, 0.19, 0.06]
 
         # Setup subscriber
         self.caster_joints = rospy.Subscriber('/caster_joints', FloatArray, self.caster_joints_callback)
@@ -30,51 +39,38 @@ class PlotCasterJoints:
         # Setup publisher
         self.pub_twist = rospy.Publisher('/cmd_vel', Twist, queue_size=20)
 
-        self.r = rospy.Rate(100)
+        self.r = rospy.Rate(self.rate)
 
-        # Initial movement to orient caster wheels randomly
-        self.move_wheelchair_init()
+        # Initialize (wait for caster joints to be subscribed)
+        self.initialize()
 
-        # Move wheelchair straight
+        # Move wheelchair
         self.move_wheelchair()
 
-        # Plot simulated caster joints data
-        self.plot_sim_data()
+        # Plot caster joints data
+        self.plot_data()
 
-        # Plot estimated caster joints data
-        # self.plot_est_data()
+        
+
         
     def caster_joints_callback(self, caster_joints):
         self.l_caster_angle, self.r_caster_angle = caster_joints.data[0], caster_joints.data[1]
         
 
-    def move_wheelchair_init(self):
-        self.wheel_cmd.linear.x = random.uniform(-0.8, 0.8)
-        self.wheel_cmd.angular.z = random.uniform(-0.5, 0.5)
+    def initialize(self):
+        rospy.loginfo("Initializing...")        
+        rospy.sleep(1)
+        self.print_caster_joints()
+
+    def move_wheelchair(self):
+        self.wheel_cmd.linear.x = 0.3
+        self.wheel_cmd.angular.z = 0.0
 
         while rospy.get_time() == 0.0:
             continue
         start = rospy.get_time()
 
-        rospy.loginfo("Initial random movement...")
-        while (rospy.get_time() - start < random.uniform(4.0, 6.0)) and not rospy.is_shutdown():
-            self.pub_twist.publish(self.wheel_cmd)
-            
-            self.r.sleep()
-
-        # Stop the robot
-        self.pub_twist.publish(Twist())
-        
-        rospy.sleep(2)
-        self.print_caster_joints()
-
-    def move_wheelchair(self):
-        self.wheel_cmd.linear.x = 0.5
-        self.wheel_cmd.angular.z = 0.0
-
-        start = rospy.get_time()
-
-        rospy.loginfo("Second movement...")
+        rospy.loginfo("Moving robot...")
         while (rospy.get_time() - start < self.move_time) and not rospy.is_shutdown():
             self.save_data()
             self.pub_twist.publish(self.wheel_cmd)        
@@ -83,17 +79,61 @@ class PlotCasterJoints:
         # Stop the robot
         self.pub_twist.publish(Twist())
         self.print_caster_joints()
+        rospy.sleep(1)
         
 
     def save_data(self):
         self.l_caster_data.append(self.l_caster_angle)
         self.r_caster_data.append(self.r_caster_angle)
 
-    def plot_sim_data(self):
-        plt.plot(self.l_caster_data)
+    def plot_data(self):
+        plt.figure(1)
+        plt.subplot(211)
+        plt.title("Left caster orientations")
+        plt.plot(self.l_caster_data, label="sim")
+        self.ode_int(self.l_caster_data)
+        plt.plot(self.sol, label="est")
+        plt.legend()
+
+        # plt.subplot(412)
+        # data_temp = []
+        # for i in range(len(self.l_caster_data)-1):
+        #     data_temp.append(self.l_caster_data[i] - self.sol[i])
+        # plt.plot(data_temp, label="error left")
+        # plt.legend()
+
+        plt.subplot(212)
+        plt.title("Right caster orientations")
+        plt.plot(self.r_caster_data, label="sim")
+        self.ode_int(self.r_caster_data)
+        plt.plot(self.sol, label="est")
+
+        # plt.subplot(414)
+        # data_temp = []
+        # for i in range(len(self.r_caster_data)-1):
+        #     data_temp.append(self.r_caster_data[i] - self.sol[i])
+        # plt.plot(data_temp, label="error right")
+        # plt.legend()
+
         plt.show()
 
-    # def plot_est_data(self):
+    def solvr(self, Y, t):
+        th = self.wheel_cmd.angular.z
+        x = -self.wheel_cmd.linear.x
+        dl = self.wh_consts[0]
+        df = self.wh_consts[1]
+        dc = self.wh_consts[2]
+        return [(th*(dl*cos(Y[0]) + (df*sin(Y[0])/2) - dc)/dc) - (x*sin(Y[0])/dc)]
+
+    def ode_int(self, lr):
+        a_t = np.arange(0.0, self.move_time, 1./self.rate)
+        ini_val = self.angle_adj(lr[0]+3.14)
+        asol = odeint(self.solvr, [ini_val], a_t)
+        self.sol = [(self.angle_adj(item+3.14)) for sublist in asol.tolist() for item in sublist]
+
+
+    def angle_adj(self, angle):
+        return atan2(sin(angle), cos(angle))
 
     def print_caster_joints(self):
         print 'Left caster: {:.3f} rad'.format(self.l_caster_angle)
