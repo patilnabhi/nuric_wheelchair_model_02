e897882581f57a4203d66945026ec86b8d12767a
Abhishek Patil <patilnabhi@gmail.com>
2016-11-27 14:29:45 -0600

    updated ukf_wheelchair2.py

--- src/ukf_wheelchair2.py
+++ src/ukf_wheelchair2.py
@@ -8,9 +8,11 @@ from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist
 from nuric_wheelchair_model_02.msg import FloatArray
 from tf.transformations import euler_from_quaternion
-import numpy as np 
+import numpy as np
 from scipy.integrate import odeint, ode
 from math import sin, cos
+import matplotlib.pyplot as plt
+
 
 class UKFWheelchair2(object):
 
@@ -21,17 +23,16 @@ class UKFWheelchair2(object):
 
         self.wheel_cmd = Twist()
 
-        self.wheel_cmd.linear.x = 0.3 
+        self.wheel_cmd.linear.x = -0.3 
         self.wheel_cmd.angular.z = 0.2
 
 
-        self.move_time = 10.0
+        self.move_time = 3.0
         self.rate = 50
         self.factor = 10
         self.dt = self.factor*1.0/self.rate 
 
-        self.l_caster_data = []
-        self.r_caster_data = []
+        self.save_caster_data = []
 
         self.asol = []
 
@@ -53,13 +54,19 @@ class UKFWheelchair2(object):
 
         self.ukf_move_wheelchair()
 
-        # self.plot_data()
+        self.caster_model()
+
+        self.plot_data()
+
+
 
     def caster_cb(self, caster_joints):       
         self.l_caster_angle, self.r_caster_angle = caster_joints.data[0], caster_joints.data[1]
 
     def odom_cb(self, odom_data):
         self.odom_vx, self.odom_vth = odom_data.twist.twist.linear.x, odom_data.twist.twist.angular.z
+        (_,_,yaw) = euler_from_quaternion([odom_data.pose.pose.orientation.x, odom_data.pose.pose.orientation.y, odom_data.pose.pose.orientation.z, odom_data.pose.pose.orientation.w])
+        self.odom_x, self.odom_th = odom_data.pose.pose.position.x, yaw
 
 
     def ukf_move_wheelchair(self):
@@ -75,7 +82,7 @@ class UKFWheelchair2(object):
             self.prev_alpha1 = x[0]
             self.prev_alpha2 = x[1]
 
-            return self.caster_model(x, dt)
+            return self.caster_model_ukf(x, dt)
 
         def hx(x):
             # print "2: ", self.prev_alpha1
@@ -87,11 +94,12 @@ class UKFWheelchair2(object):
             sol = self.meas_model(x[0], x[1], alpha1dot, alpha2dot)
             return sol
         
-        points = MerweScaledSigmaPoints(n=2, alpha=.01, beta=1., kappa=-1.)
+        points = MerweScaledSigmaPoints(n=2, alpha=.1, beta=1., kappa=-1.)
         kf = UKF(dim_x=2, dim_z=2, dt=self.dt, fx=fx, hx=hx, points=points, sqrt_fn=None, x_mean_fn=self.state_mean, z_mean_fn=self.meas_mean, residual_x=self.residual_x, residual_z=self.residual_z)
 
         # self.ini_val = [normalize_angle(self.l_caster_angle-np.pi), normalize_angle(self.r_caster_angle-np.pi)]
-        self.ini_val = [1.3, -3.14]
+        self.ini_val = [3.1, -3.14]
+        self.save_caster_data.append(self.ini_val)
 
         kf.x = np.array(self.ini_val)   # initial mean state
         kf.P *= 0.0001  # kf.P = eye(dim_x) ; adjust covariances if necessary
@@ -101,15 +109,24 @@ class UKFWheelchair2(object):
         zs = []
         xs = []
 
+        # xs.append(self.ini_val)
+
         count = 0
 
-        print "Est1: ", normalize_angle(kf.x[0]+np.pi), normalize_angle(kf.x[1]+np.pi) 
+        # print "Est1: ", normalize_angle(kf.x[0]+np.pi), normalize_angle(kf.x[1]+np.pi) 
 
         rospy.loginfo("Moving robot...")
+        
+        last_odom_x = self.odom_x
+        last_odom_th = self.odom_th
         start = rospy.get_time()
+        self.r.sleep()
+        
         while (rospy.get_time() - start < self.move_time) and not rospy.is_shutdown():
-
-            z = np.array([self.odom_vx, self.odom_vth])
+            curr_odom_x, curr_odom_th = self.odom_x, self.odom_th
+            delta_x, delta_th = curr_odom_x - last_odom_x, curr_odom_th - last_odom_th
+            z = np.array([delta_x/self.dt, delta_th/self.dt])
+            # z = np.array([self.odom_vx, self.odom_vth])
             
             if count%self.factor==0:
 
@@ -119,22 +136,27 @@ class UKFWheelchair2(object):
                 kf.update(z)
 
                 xs.append([normalize_angle(kf.x[0]+np.pi), normalize_angle(kf.x[1]+np.pi)])
- 
-                print "Est: ", normalize_angle(kf.x[0]+np.pi), normalize_angle(kf.x[1]+np.pi)
-                print "Act: ", normalize_angle(self.l_caster_angle), normalize_angle(self.r_caster_angle)
+    
 
-                self.l_caster_data.append(self.l_caster_angle)
-                self.r_caster_data.append(self.r_caster_angle)
+                # print "Est: ", normalize_angle(kf.x[0]+np.pi), normalize_angle(kf.x[1]+np.pi)
+                # print "Act: ", normalize_angle(self.l_caster_angle), normalize_angle(self.r_caster_angle)
+
+            self.save_caster_data.append([self.l_caster_angle, self.r_caster_angle])
 
 
             self.pub_twist.publish(self.wheel_cmd)    
             
             count += 1
+            last_odom_x, last_odom_th = curr_odom_x, curr_odom_th
             self.r.sleep()
 
 
         # Stop the robot
         self.pub_twist.publish(Twist())
+
+        self.caster_sol_ukf = np.array(xs)
+        self.caster_sol_act = np.array(self.save_caster_data)
+
         rospy.sleep(1)
 
     def solvr_caster_model(self, x, t, vx=None, vth=None):
@@ -154,16 +176,25 @@ class UKFWheelchair2(object):
         return [eq1, eq2]
 
 
-    def caster_model(self, x0, dt):
+    def caster_model_ukf(self, x0, dt):
 
-        a_t = np.arange(0.0, dt, 0.01)
+        a_t = np.arange(0.0, dt, 0.001)
         # ini_val = [normalize_angle(self.r_caster_angle-np.pi), normalize_angle(self.l_caster_angle-np.pi)]
         ini_val = x0
         asol = odeint(self.solvr_caster_model, ini_val, a_t)
         
-        # self.asol = asol
         
         return asol[-1]
+
+
+    def caster_model(self):
+
+        a_t = np.arange(0.0, self.move_time, self.dt)
+        ini_val = self.ini_val
+        asol = odeint(self.solvr_caster_model, ini_val, a_t)
+
+
+        self.caster_sol = asol
 
 
     def meas_model(self, alpha1, alpha2, alpha1dot, alpha2dot):
@@ -250,56 +281,84 @@ class UKFWheelchair2(object):
             
         return y
     
-    def calc_error(self):
+    def calc_error(self, data1, data2):
 
-        for i in xrange(min(len(self.sol_alpha1), len(self.r_caster_data))):
-            self.error_alpha1.append(self.sol_alpha1[i]-self.r_caster_data[i])
+        error = []
 
-        for i in xrange(min(len(self.sol_alpha2), len(self.l_caster_data))):
-            self.error_alpha2.append(self.sol_alpha2[i]-self.l_caster_data[i])
+        for i in xrange(min(len(data1), len(data2))):
+            error.append(data1[i]-data2[i])
+
+        return error
+
+        # for i in xrange(min(len(self.sol_alpha2), len(self.l_caster_data))):
+        #     self.error_alpha2.append(self.sol_alpha2[i]-self.l_caster_data[i])
+
+    def generate_data(self, data, side):
+
+        out_data = []
+
+        if side == 'left':
+            side = 0
+        elif side == 'right':
+            side = 1
+        else:
+            print "Check arguments!"
+            return
+
+        for i in xrange(len(data)):
+            out_data.append(data[i,side])
+
+        return out_data
 
     def plot_data(self):
 
-        self.ode_int()
 
-        for i in xrange(len(self.asol)):
-            self.sol_alpha1.append(normalize_angle(self.asol[i][0]))
-            self.sol_alpha2.append(normalize_angle(self.asol[i][1]))
+        # self.calc_error()
 
 
-        self.sol_alpha1 = [normalize_angle((angle+np.pi)) for angle in self.sol_alpha1]
-        self.sol_alpha2 = [normalize_angle((angle+np.pi)) for angle in self.sol_alpha2]
-
-        self.calc_error()
-
+        
 
         plt.figure(1)
 
+        # Plot left caster
         plt.subplot(221)
-        plt.title("R Caster Orientation (rad)")
-        xaxis = [x/100. for x in xrange(len(self.sol_alpha1))]
-        plt.plot(xaxis, self.sol_alpha1, label="est")
-        xaxis = [x/100. for x in xrange(len(self.r_caster_data))]
-        plt.plot(xaxis, self.r_caster_data, label="actual")
+        plt.title("L Caster Orientation (rad)")
+
+        data = self.generate_data(self.caster_sol, 'left')
+        data_est = [normalize_angle(angle+np.pi) for angle in data]
+        xaxis = [x/5. for x in xrange(len(data_est))]
+        plt.plot(xaxis, data_est, label="est")
+
+        data_ukf = self.generate_data(self.caster_sol_ukf, 'left')
+        xaxis = [x/5. for x in xrange(len(data_ukf))]
+        plt.plot(xaxis, data_ukf, label="ukf")
+
+        data = self.generate_data(self.caster_sol_act, 'left')
+        xaxis = [x/self.rate for x in xrange(len(data))]
+        plt.plot(xaxis, data, label="act")
+
         plt.legend()
 
-        plt.subplot(222)
-        plt.title("L Caster Orientation (rad)")
-        xaxis = [x/100. for x in xrange(len(self.sol_alpha2))]
-        plt.plot(xaxis, self.sol_alpha2, label="est")
-        xaxis = [x/100. for x in xrange(len(self.l_caster_data))]
-        plt.plot(xaxis, self.l_caster_data, label="actual")
-        plt.legend()
+
+
+        # plt.subplot(222)
+        # plt.title("L Caster Orientation (rad)")
+        # xaxis = [x/100. for x in xrange(len(self.sol_alpha2))]
+        # plt.plot(xaxis, self.sol_alpha2, label="est")
+        # xaxis = [x/100. for x in xrange(len(self.l_caster_data))]
+        # plt.plot(xaxis, self.l_caster_data, label="actual")
+        # plt.legend()
 
         plt.subplot(223)
-        plt.title("Error R Caster Orientation (rad)")
-        xaxis = [x/100. for x in xrange(len(self.error_alpha1))]
-        plt.plot(xaxis, self.error_alpha1)
-
-        plt.subplot(224)
         plt.title("Error L Caster Orientation (rad)")
-        xaxis = [x/100. for x in xrange(len(self.error_alpha2))]
-        plt.plot(xaxis, self.error_alpha2)
+        error_data = self.calc_error(data_est, data_ukf)
+        xaxis = [x/5. for x in xrange(len(error_data))]
+        plt.plot(xaxis, error_data)
+
+        # plt.subplot(224)
+        # plt.title("Error L Caster Orientation (rad)")
+        # xaxis = [x/100. for x in xrange(len(self.error_alpha2))]
+        # plt.plot(xaxis, self.error_alpha2)
 
         plt.show()
 
